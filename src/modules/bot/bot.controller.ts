2c6c35516cad9f69437af19b0601060f0f5cec55
Jerry <JerryTech.Y@outlook.com>
2025-05-06 13:55:36 +0800

    新增获取群聊消息路由

--- src/modules/bot/bot.controller.ts
+++ src/modules/bot/bot.controller.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import response from '../../utils/core/response';
 import BotService from './bot.service';
 import tools from '../../utils/modules/tools';
+import logger from '../../utils/core/logger';
 
 class BotController {
   private readonly router: express.Router;
@@ -17,6 +18,7 @@ class BotController {
 
   private init(): void {
     this.router.post(`/getBotId`, this.postBotsId);
+    this.router.post('/getBotInfo', this.postGroupInfo);
   }
 
   /**
@@ -35,6 +37,38 @@ class BotController {
       }
     } catch (err) {
       await response.error(res, `请求失败..`, 500, err);
+    }
+  };
+
+  /**
+   * 获取群聊信息
+   * @example req示例
+   * ```json
+   * {
+   * token: ‘114514’,
+   * botId: ‘114514’,
+   * groupId: ‘114514’
+   * }
+   * ```
+   * @param req
+   * @param res
+   */
+  private postGroupInfo = async (req: express.Request, res: express.Response): Promise<void> => {
+    try {
+      const token = req.body.token;
+      if (tools.checkToken(token.toString())) {
+        const botId = req.body.botId;
+        const groupId = req.body.groupId;
+        let returnData = await BotService.getGroupInfo({ botId: botId, groupId: groupId });
+        if (returnData) {
+          await response.success(res, returnData);
+          logger.debug(returnData);
+        }
+      } else {
+        await tools.tokenCheckFailed(res, token);
+      }
+    } catch (e) {
+      await response.error(res);
     }
   };
 }
